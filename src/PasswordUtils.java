b05a608fc5ba36369915f5ffcee66b6c9e490bc7
Auberon López <auberonedu@gmail.com>
2025-04-16 08:24:04 -0700

    Initial implementation

---
+++ src/PasswordUtils.java
@@ -0,0 +1,41 @@
+/**
+ * Utility class for analyzing passwords.
+ */
+public class PasswordUtils {
+
+    /**
+     * Returns a description of the password's length.
+     *
+     * @param password the password to analyze
+     * @return "short" if the password has fewer than 6 characters,
+     *         "medium" if it has between 6 and 11 characters (inclusive),
+     *         or "long" if it has 12 or more characters
+     */
+    public static String describePasswordLength(String password) {
+        int length = password.length();
+        if (length < 6) {
+            return "short";
+        } else if (length <= 12) {
+            return "medium";
+        }
+        return "long";
+    }
+
+    /**
+     * Checks whether the password contains only letters and digits.
+     *
+     * @param password the password to check
+     * @return true if the password is alphanumeric, false otherwise
+     */
+    public static boolean isAlphanumeric(String password) {
+        for (int i = 0; i < password.length() - 1; i++) {
+            char c = password.charAt(i);
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return true;
+    }
+}
