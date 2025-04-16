744f33549412efc9e4a23383e9284d9babee50f7
Auberon López <auberonedu@gmail.com>
2025-04-16 09:22:49 -0700

    Add unimplemented methods

--- src/PasswordUtils.java
+++ src/PasswordUtils.java
@@ -38,4 +38,52 @@ public class PasswordUtils {
         }
         return true;
     }
+
+
+    /**
+     * Checks whether a password has a character that is repeated at least three
+     * times in a row.
+     * 
+     * For example, "paaasword" has the letter 'a' three times in a row.
+     * 
+     * @param password the password to check
+     * @return true if password has a character repeated three times in a row, false otherwise 
+     */
+    public static boolean containsTriple(String password) {
+        // TODO: Make tests FIRST, then implement the method
+        // Please have your tests in a separate commit from the implementation
+        return false;
+    }
+
+    /**
+     * Returns the number of special characters in the password.
+     * 
+     * A special character is any non-alphanumeric (not letter or digit) character.
+     * 
+     * 
+     * @param password the password to check
+     * @return the count of special characters
+     */
+    public static int countSpecialCharacters(String password) {
+        // TODO: Make tests FIRST, then implement the method
+        // Please have your tests in a separate commit from the implementation
+        return 0;
+    }
+
+    /**
+     * Checks whether a password has at least a minimum number of special characters.
+     * 
+     * A special character is any non-alphanumeric (not letter or digit) character.
+     * 
+     * @param password the password to check
+     * @param minimum the minimum number of special characters (inclusive)
+     * @return true has the minimum number of special charcters or more, false otherwise
+     */
+    public static boolean hasSufficientSpecialCharacters(String password, int minimum) {
+        // TODO: Make tests FIRST, then implement the method
+        // Please have your tests in a separate commit from the implementation
+        // Required: please use countSpecialCharacters as a helper method.
+        // Don't just copy/paste the logic!
+        return false;
+    }
 }
